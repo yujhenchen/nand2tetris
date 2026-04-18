329f89c8c618684f544932e06d75d768cf05521b
yujhenchen <yujhen.chen93@gmail.com>
2026-04-18 14:21:41 +0800

    feat: implement clear and add fill structure

--- project4/Fill.asm
+++ project4/Fill.asm
@@ -1,5 +1,10 @@
+// M always means RAM[A], for example, A=16, M means RAM[16]
 // read from keyboard, write to screen
-// loop (true) { if key === 0, clear screen; else write screen}
+// Screen memory ends at: 24575
+// KBD = 24576
+
+
+
 
 
 // loop label
@@ -9,26 +14,81 @@
 @KBD
 D=M
 
-// if the key is 0, load to D register, set to 0
-// else, set to key
+// if the key is 0, load to D register, jump to clear
+// else, jump to fill
 @CLEAR
 D;JEQ
 
 @FILL
 D;JNE
 
+// clear label
 (CLEAR)
+
+// select the address of the screen and load into D register
 @SCREEN
-D=0
+D=A
+// allocate a memory for the variable pointer, and select the memory
+// (which loads the memory address automatically into A register)
+@pointer
+// assign the screen address stores in D now to the selected memory
+// (which becomes the value of pointer)
 M=D
 
-// jump tp loop
+// label for inner loop clear screen
+(CLEAR_LOOP)
+
+// select the pointer variable , now A = address of variable "pointer"
+@pointer
+// make A become the value stored in pointer
+A=M
+// clear the memory at RAM[pointer]
+M=0
+
+// select the pointer, so that M is the value of the memory that stores in pointer
+// (which points to the current screen memory),
+// and load the memory value into D register
+@pointer
+D=M
+// D = pointer - KBD
+@KBD
+D=D-M
+// has the pointer reached the keyboard memory boundary? (D-M is 0 or not)
+// (if pointer == KBD → we reached end of screen memory → exit loop)
+@EXIT
+D;JEQ
+
+// else, increase the value of pointer and write it back to pointer
+@pointer
+M=M+1
+@CLEAR_LOOP
+0;JMP
+
+
+// jump to the main loop
+(EXIT)
 @LOOP
 0;JMP
 
-// a loop over all screen addresses
+
 (FILL)
+// init pointer to point to the address of the first memory of the screen
+// inner loop to fill the screen if not pointer has not reached the keyboard memory boundary
+@SCREEN
+D=A
+@pointer
 M=D
+
+// calculate D
+
+@EXIT
+D;JEQ
+
+@FILL_LOOP
+D;JEQ
+
+(FILL_LOOP)
+
 
 // jump tp loop
 @LOOP
